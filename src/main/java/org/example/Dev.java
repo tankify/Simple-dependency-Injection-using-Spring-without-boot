b64b60cd5878818330703774d4ae8638fd513a68
Chirag Tank <tankchirag22@gmail.com>
2025-11-20 13:50:02 +0530

    Spring is assigning age vaule or injecting age value using xml file

---
+++ src/main/java/org/example/Dev.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Dev {
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    //private Laptop laptop;
+    private int age;
+
+    public Dev(){
+        System.out.println("Dev Constructor");
+    }
+
+    public void build()
+    {
+        System.out.println("learning Spring");
+        //laptop.compile();
+    }
+}
